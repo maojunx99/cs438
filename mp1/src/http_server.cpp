901c80dc24af2f2fe869ce0c249149736034cfd4
maojunx <maojunx99.gmail.com>
2022-09-18 00:36:51 -0500

    incremented version to 10

--- mp1/src/http_server.cpp
+++ mp1/src/http_server.cpp
@@ -1,82 +1,44 @@
-//
-// Created by ASUS on 2022/9/5.
-//
-#include <cstdio>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <cstring>
-#include <cerrno>
-#include <arpa/inet.h>
-#include <iostream>
-#include <algorithm>
-#include <iterator>
-#include <cstdio>
-#include <memory>
-#include <stdexcept>
-#include <array>
-#include <vector>
-#include <sstream>
-using namespace std;
-
-//TODO: To check when DATA_BUFFER_SIZE is exceeded, whether bad thing will happen
-#define DATA_BUFFER_SIZE 5000
-#define BUFFER_SIZE 4096
-#define MAX_CONNECTIONS 10
-enum {
-    RESULT_NORMAL,
-    /* file errors */
-    ERROR_FILE_NOT_EXIST,
-
-    ERROR_COMMAND,
-    ERROR_WRONG_PARAMETER,
-    ERROR_MISS_PARAMETER,
-    ERROR_MISS_PROGRAM,
-    ERROR_TOO_MANY_ARGUMENTS,
-    ERROR_SYNTAX,
-    ERROR_SYSTEM,
-    ERROR_EXIT,
-
-};
 /*
-using std::cout; using std::endl;
-using std::string; using std::reverse;
-int CHAR_LENGTH = 1;*/
+** server.c -- a stream socket server demo
+*/
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <sstream>
+using namespace  std;
+//#define PORT "3490"  // the port users will be connecting to
 
-int create_tcp_server_socket(int port) {
-    struct sockaddr_in saddr;
-    int fd, ret_val;
-
-    /* Step1: create a TCP socket */
-    fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (fd == -1) {
-        fprintf(stderr, "socket failed [%s]\n", strerror(errno));
-        return -1;
-    }
-    //printf("Created a socket with fd: %d\n", fd);//9-11
-
-    /* Initialize the socket address structure */
-    saddr.sin_family = AF_INET;
-    saddr.sin_port = htons(port);
-    saddr.sin_addr.s_addr = INADDR_ANY;
-
-    /* Step2: bind the socket to port 7000 on the local host */
-    ret_val = bind(fd, (struct sockaddr *)&saddr, sizeof(struct sockaddr_in));
-    if (ret_val != 0) {
-        fprintf(stderr, "bind failed [%s]\n", strerror(errno));
-        close(fd);
-        return -1;
-    }
-
-    /* Step3: listen for incoming connections */
-    ret_val = listen(fd, 5);
-    if (ret_val != 0) {
-        fprintf(stderr, "listen failed [%s]\n", strerror(errno));
-        close(fd);
-        return -1;
-    }
-    return fd;
+#define BACKLOG 10	 // how many pending connections queue will hold
+#define DATA_BUFFER_SIZE 5000
+#define BUFFER_SIZE 1024
+void sigchld_handler(int s){
+	while(waitpid(-1, NULL, WNOHANG) > 0);
 }
-string return_file(const char* s,int fd){
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa){
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+
+string return_file(string s,int fd){
 
     //const char* log = LOG_FILE_DIR;
     std::string command_str= s;
@@ -121,7 +83,7 @@ string return_file(const char* s,int fd){
         long long byte_count=0;
         cout<<"first word is GET"<<endl;
         string file_directory_str(words[1],1);
-        fp = fopen(file_directory_str.c_str(), "r");
+        fp = fopen(file_directory_str.c_str(), "rb");
         if (fp == NULL) {
             const char* notfound_msg="HTTP/1.1 404 Not Found\r\n\r\n";
             int notfound_msg_return_result=send(fd,notfound_msg,strlen(notfound_msg),0);
@@ -137,24 +99,48 @@ string return_file(const char* s,int fd){
         }
 
         //int blank_line_return_result=send(fd,"\r\n",strlen("\r\n"),0);
+        /*
         while (fgets(buffer, sizeof(buffer), (FILE*)fp)) {
             ret_val_grep_result=send(fd,buffer,sizeof(buffer),0);
             byte_count+=ret_val_grep_result;
             //cout<<"Sent:"<<buffer<<endl;
             result += buffer;//.data();
             count++;
+        }*/
+        while(!feof(fp)) {
+            size_t ret_value = fread(buffer, sizeof(char), BUFFER_SIZE, fp);
+            result+=buffer;
+            if(ret_value <= 0) {
+                printf("finish reading.\n");
+                break;
+            } else if(ret_value == BUFFER_SIZE){ // error handling
+                printf("reading file\n");
+                if (send(fd, buffer, BUFFER_SIZE, 0) == -1){
+                    //result+=buffer;
+                    perror("send");
+                }
+
+            } else {
+                printf("reading file\n");
+                if (send(fd, buffer, (int)ret_value, 0) == -1){
+                    perror("send");
+                }
+
+            }
         }
+
+
         cout<<"total byte sent:"<<byte_count<<endl;
         cout<<"ret_val_grep_result="<<ret_val_grep_result<<endl;
         if(ret_val_grep_result==0){
             cout<<"request content sent successfully"<<endl;
         }
-        char null_buffer[2]=" ";
+        /*char null_buffer[2]=" ";
         //sleep(1);
         ret_val_grep_result=send(fd,null_buffer,sizeof(null_buffer),0);
         if(ret_val_grep_result==0){
             cout<<"final byte sent successfully"<<endl;
-        }
+        }*/
     }else{
         const char* bad_request_msg="400 Not Found\r\n\r\n";
         int bad_request_msg_return_result=send(fd,bad_request_msg,strlen(bad_request_msg),0);
@@ -163,115 +149,122 @@ string return_file(const char* s,int fd){
         }
     }
 
+    fclose(fp);
+    cout<<"finish sending whole file"<<endl;
     return result;
 }
-int main (int argc, char** argv) {
+
+int main(int argc, char** argv){
     string port_str=argv[1];
     int port=atoi(port_str.c_str());
+	int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+	struct addrinfo hints, *servinfo, *p;
+	struct sockaddr_storage their_addr; // connector's address information
+	socklen_t sin_size;
+	struct sigaction sa;
+	int yes=1;
+	char s[INET6_ADDRSTRLEN];
+	int rv,ret_val;
 
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    fd_set read_fd_set;
-    struct sockaddr_in new_addr;
-    int server_fd, new_fd, ret_val, i;
-    socklen_t addrlen;
-    char buf[DATA_BUFFER_SIZE];
-    int all_connections[MAX_CONNECTIONS];
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE; // use my IP
 
-    /* Get the socket server fd */
-    server_fd = create_tcp_server_socket(port);
-    if (server_fd == -1) {
-        fprintf(stderr, "Failed to create a server\n");
-        //close(server_fd);
-        /*
-        shutdown(server_fd,SHUT_RDWR);
-        printf("Now closed the original server.");
-        server_fd = create_tcp_server_socket();*/
-        return -1;
-    }
+	if ((rv = getaddrinfo(NULL, port_str.c_str(), &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
 
-    /* Initialize all_connections and set the first entry to server fd */
-    for (i=0;i < MAX_CONNECTIONS;i++) {
-        all_connections[i] = -1;
-    }
-    all_connections[0] = server_fd;
+	// loop through all the results and bind to the first we can
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+				p->ai_protocol)) == -1) {
+			perror("server: socket");
+			continue;
+		}
 
-    while (1) {
-        FD_ZERO(&read_fd_set);
-        /* Set the fd_set before passing it to the select call */
-        for (i=0;i < MAX_CONNECTIONS;i++) {
-            if (all_connections[i] >= 0) {
-                FD_SET(all_connections[i], &read_fd_set);
+		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+				sizeof(int)) == -1) {
+			perror("setsockopt");
+			exit(1);
+		}
+
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("server: bind");
+			continue;
+		}
+
+		break;
+	}
+
+	if (p == NULL)  {
+		fprintf(stderr, "server: failed to bind\n");
+		return 2;
+	}
+
+	freeaddrinfo(servinfo); // all done with this structure
+
+	if (listen(sockfd, BACKLOG) == -1) {
+		perror("listen");
+		exit(1);
+	}
+
+	sa.sa_handler = sigchld_handler; // reap all dead processes
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+		perror("sigaction");
+		exit(1);
+	}
+
+	printf("server: waiting for connections...\n");
+
+	while(1) {  // main accept() loop
+		sin_size = sizeof their_addr;
+		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+		if (new_fd == -1) {
+			perror("accept");
+			continue;
+		}
+
+		inet_ntop(their_addr.ss_family,
+			get_in_addr((struct sockaddr *)&their_addr),
+			s, sizeof s);
+		printf("server: got connection from %s\n", s);
+
+		if (!fork()) { // this is the child process
+            close(sockfd); // child doesn't need the listener
+		    /*********************************added***************************************/
+		    char buf[DATA_BUFFER_SIZE];
+            ret_val = recv(new_fd, buf, DATA_BUFFER_SIZE, 0);
+            if (ret_val == 0) {
+                printf("Closing connection for fd:%d\n", new_fd);//9-11
+                close(new_fd);
+                new_fd = -1; /* Connection is now closed */
             }
-        }
-
-        /* Invoke select() and then wait! */
-        //printf("\nUsing select() to listen for incoming events\n");//9-11
-        ret_val = select(FD_SETSIZE, &read_fd_set, NULL, NULL, NULL);
-
-        /* select() woke up. Identify the fd that has events */
-        if (ret_val >= 0 ) {
-            //printf("Select returned with %d\n", ret_val);//9-11
-            /* Check if the fd with event is the server fd */
-            if (FD_ISSET(server_fd, &read_fd_set)) {
-                /* accept the new connection */
-                //printf("Returned fd is %d (server's fd)\n", server_fd);//9-11
-                new_fd = accept(server_fd, (struct sockaddr*)&new_addr, &addrlen);
-                if (new_fd >= 0) {
-                    //printf("Accepted a new connection with fd: %d\n", new_fd);//9-11
-                    for (i=0;i < MAX_CONNECTIONS;i++) {
-                        if (all_connections[i] < 0) {
-                            all_connections[i] = new_fd;
-                            break;
-                        }
-                    }
-                } else {
-                    fprintf(stderr, "accept failed [%s]\n", strerror(errno));
-                }
-                ret_val--;
-                if (!ret_val) continue;
+            if (ret_val > 0) {
+                std::cout<<"buf="<<buf<<endl;
+                string ans = return_file(buf,new_fd);
+                bzero(buf,DATA_BUFFER_SIZE);
             }
+            if (ret_val == -1) {
+                printf("recv() failed for fd: %d [%s]\n",new_fd, strerror(errno));
+                //break;
+            }
+            /*********************************above added***************************************/
 
-            /* Check if the fd with event is a non-server fd */
-            for (i=1;i < MAX_CONNECTIONS;i++) {
-                if ((all_connections[i] > 0) &&
-                    (FD_ISSET(all_connections[i], &read_fd_set))) {
-                    /* read incoming data */
-                    //printf("Returned fd is %d [index, i: %d]\n", all_connections[i], i);//9-11
-                    ret_val = recv(all_connections[i], buf, DATA_BUFFER_SIZE, 0);
-                    if (ret_val == 0) {
-                        //printf("Closing connection for fd:%d\n", all_connections[i]);//9-11
-                        close(all_connections[i]);
-                        all_connections[i] = -1; /* Connection is now closed */
-                    }
-                    if (ret_val > 0) {
-                        //printf("Received data (len %d bytes, fd: %d): %s\n",ret_val, all_connections[i], buf);//9-11
+			/*if (send(new_fd, "Hello, world!", 13, 0) == -1){
+                perror("send");
+			}*/
 
-                        //std::cout<<"456";
-                        std::cout<<"buf="<<buf<<endl;
-                        string ans = return_file(buf,all_connections[i]);
-                        //std::cout<<"ans is"<<endl;
-                        //std::cout<<ans<<endl;
-                        bzero(buf,BUFFER_SIZE);
-                    }
-                    if (ret_val == -1) {
-                        printf("recv() failed for fd: %d [%s]\n",
-                               all_connections[i], strerror(errno));
-                        break;
-                    }
-                }
-                ret_val--;
-                if (!ret_val) continue;
-            } /* for-loop */
-        } /* (ret_val >= 0) */
-    } /* while(1) */
+			close(new_fd);
+			exit(0);
+		}
+		close(new_fd);  // parent doesn't need this
+	}
 
-    /* Last step: Close all the sockets */
-    for (i=0;i < MAX_CONNECTIONS;i++) {
-        if (all_connections[i] > 0) {
-            close(all_connections[i]);
-        }
-    }
-    return 0;
+	return 0;
 }
+
