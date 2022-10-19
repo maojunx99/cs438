4977e1b2bfcdd531d56a2319f8f333f9db8c7088
maojunx <maojunx99.gmail.com>
2022-10-19 16:32:08 -0500

    incremented version to 40

--- mp2/inc_and_push.sh
+++ mp2/inc_and_push.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-seconds=$((1800))
-for i in {21..40}
-do
-    C:/Users/XUMAOJUN/Desktop/CS_438/auto-code.py $i 
-    typeset -i version=$(cat version.txt)
+typeset -i version=$(cat version.txt)
 
-    printf "\nchanging version from $version to $((version+1))\n\n"
-    version=$((version+1))
-    echo "$version" > version.txt
+printf "\nchanging version from $version to $((version+1))\n\n"
+version=$((version+1))
+echo "$version" > version.txt
 
-    git add -u
-    git commit -m "incremented version to $version"
-    git push origin main
-    sleep ${seconds}
-done
+git add -u
+git commit -m "incremented version to $version"
+git push origin main
+sleep ${seconds}
